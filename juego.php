ad223240b232d751288e2b71a2ba77747f299e0a
Oualid Amallah <walidamallah@gmail.com>
2018-01-12 09:07:59 +0100

    ahorcado falta perdir desc

--- juego.php
+++ juego.php
@@ -2,40 +2,65 @@
 
 $incognita = htmlspecialchars($_POST['incognita']);
 $letra=isset($_POST['letra']) ? htmlspecialchars($_POST['letra']):'';
+
+$vidas=isset($_POST['vidas']) ? (int)($_POST['vidas']):7;
+
 $letras=isset($_POST['letras']) ? ($_POST['letras']):array();
-$letras[]=$letra;
+
+if(preg_match("%[a-z]%", $letra) && !in_array($letra, $letras)){
+    $letras[]=$letra;       
+}
+
 ?>
 <h1>
 <?php
+$ganar=0;
+$hasEcertado=false;
 for ($i = 0; $i < strlen($incognita); $i++) {
     $con=false;
     foreach ($letras as $letra) {
             if($incognita[$i]===$letra){
                 echo $letra . "";
                 $con=true;
+                $ganar++;
+                $hasEcertado=true;
             }
    	}
        if(!$con){
         echo ' _';
    } 
 }
+if(!$hasEcertado){
+    $vidas--;
+    print_r($vidas);
+}
 ?>
 
 </h1>
 
+<?php
+if($ganar==strlen($incognita)): 
+?>   
+   <h2>Has Ganado¡¡¡¡</h2> 
+    <a href="index.html">Intentalo Otra Vez</a>
+<?php
+    else:
+?>
 
 <form action="" method="post">
     <input type="hidden" name="incognita" value='<?php echo  $incognita ?>'/>
+    <input type="hidden" name="vidas" value='<?php echo  $vidas ?>'/>
         <?php
             for ($j = 0; $j < count($letras); $j++) :
         ?>
 
             <input type="hidden" name="letras[<?php echo $j ?>]" value="<?php echo $letras[$j] ?>" />
             <?php echo $letras[$j] ?>
-            <br>
+            
         <?php
             endfor;
         ?>
+        <br>
     <label for="letra">Escriba una LETRA</label>
     <select name="letra">
         <?php
@@ -49,3 +74,6 @@ for ($i = 0; $i < strlen($incognita); $i++) {
     
     <input type="submit" value="Enviar">
 </form>
+<?php
+    endif;
+?>
